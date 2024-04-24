310ae6465e1b9aac66b23895edac96cf60236ea1
UTOPi4T <rohmovonyonp@gmail.com>
2024-04-24 19:46:13 +0700

    Clerk kicked in

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Login from './screens/AuthScreens/Login';
+import { ClerkProvider } from "@clerk/clerk-expo";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Login/>
-      <StatusBar style="auto" />
-    </View>
+    <ClerkProvider publishableKey={'pk_test_c3VpdGFibGUtbWVlcmthdC01MC5jbGVyay5hY2NvdW50cy5kZXYk'}>
+      <View style={styles.container}>
+        <Login/>
+        <StatusBar style="auto" />
+      </View>
+    </ClerkProvider>
   );
 }
 
